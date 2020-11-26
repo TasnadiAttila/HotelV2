c6e469b319dd0dbb94f823f08f611cf02b66ed5c
TasnadiAttila <tasnadi.attila21@gmail.com>
2020-11-26 15:12:06 +0100

    Slideshow, login buxfixes, other bug fixes that i don't remember

--- main.js
+++ main.js
@@ -1,39 +1,46 @@
-let vege;
 
 function vegrehajt(){
     let login = document.querySelector("#login").value;
     let logout = document.querySelector("#logout").value;
 
-    login = Date.parse(login);
-    logout = Date.parse(logout);
-
-
-    if(login >= logout){
-        window.alert("Próbáljon már meg később kijelentkezni mint bejelentkezni, legyen szíves! Jó kössz!");
+    if (!login) {
+        window.alert("Kérjük adja meg a bejelentkezés dátumát!")
+    }
+    else if(!logout){
+        window.alert("Kérjük adja meg a kijelentkezés dátumát!")
     }
     else{
-        let milliseconds = (logout-login);
+        login = Date.parse(login);
+        logout = Date.parse(logout);
 
-        let days = (milliseconds / (1000*60*60*24));
-        
-        let naps = days.toString();
-        
-        document.getElementById('napok').innerHTML = naps + " napot.";
-       
-        let emberek = document.getElementById('szemelyekszama').value;
-    
-        let szobaar = 4000;
-
-        if(emberek <= 0){
-            window.alert("Nem lehet 0 személyre foglalni!")
-            document.getElementById("napok").innerHTML = " ";
+        if(login >= logout){
+            window.alert("Próbáljon már meg később kijelentkezni mint bejelentkezni, legyen szíves! Jó kössz!");
         }
-        
         else{
-            let vege = emberek * szobaar *naps;
-            vegee = vege.toString()
-            document.getElementById("abszolutvegeosszeg").innerHTML = "Összsen " + vegee + " Ft-ba fog kerülni.";
+            let milliseconds = (logout-login);
+    
+            let days = (milliseconds / (1000*60*60*24));
+            
+            let naps = days.toString();
+            
+            document.getElementById('napok').innerHTML = naps + " napot.";
+           
+            let emberek = document.getElementById('szemelyekszama').value;
+        
+            let szobaar = 4000;
+    
+            if(emberek <= 0){
+                window.alert("Nem lehet 0 személyre foglalni!")
+                document.getElementById("napok").innerHTML = " ";
+            }
+            
+            else{
+                let vege = emberek * szobaar *naps;
+                vegee = vege.toString()
+                document.getElementById("abszolutvegeosszeg").innerHTML = "Összsen " + vegee + " Ft-ba fog kerülni.";
+            }
         }
+
     }
 }
 
